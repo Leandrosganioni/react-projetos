09e831567f064157bb8d853507dfb4979e13c67c
Leandro Sganioni <leandrosgani@gmail.com>
2025-05-19 21:45:13 -0300

    corrigido semana 3 e 2

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,11 @@ const Home = () => {
       <nav>
         <ul>
           <li><Link to="/semana01">Semana 01</Link></li>
-          <li><Link to="/semana02">Semana 02</Link></li>
+
+          <li><Link to="/semana03">Semana 02</Link></li>
+
+          <li><Link to="/semana02">Semana 03</Link></li>
+
 
 
         </ul>
--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './Home';
-import Semana01 from './Semana01'; // nova página da semana
+import Semana01 from './Semana01'; 
 import Atv01 from './Atv01';
 import Atv02 from './Atv02';
 import Semana02 from './Semana02';
+import Semana03 from './Semana03';
 
 export default function MinhasRotas() {
   return (
@@ -15,6 +16,7 @@ export default function MinhasRotas() {
         <Route path="/atv01" element={<Atv01 />} />
         <Route path="/atv02" element={<Atv02 />} />
         <Route path="/semana02" element={<Semana02 />} />
+        <Route path="/semana03" element={<Semana03 />} />
       </Routes>
     </BrowserRouter>
   );
